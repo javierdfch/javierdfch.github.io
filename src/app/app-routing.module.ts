81d63d1056c0fe0c263474833c94b13af64f673b
MyPorta <ingjferc@gmail.com>
2023-09-13 15:45:42 -0500

    mycomit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio/inicio.component';
+import { ContactenosComponent } from './pages/contactenos/contactenos.component';
+import { ProjectComponent } from './pages/project/project.component';
+import { ResumeComponent } from './pages/resume/resume.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'contactenos', component: ContactenosComponent },
+  { path: 'project', component: ProjectComponent},
+  { path: 'resume', component: ResumeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactenosComponent } from './pages/contactenos/contactenos.component';
+import { InicioComponent } from './pages/inicio/inicio/inicio.component';
+import { ProjectComponent } from './pages/project/project.component';
+import { ResumeComponent } from './pages/resume/resume.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactenosComponent,
+    InicioComponent,
+    ProjectComponent,
+    ResumeComponent
   ],
   imports: [
     BrowserModule,
